b4dd14294d42100b24e95c4895d96f501bce0491
r4d0v1c <lukarad9@gmail.com>
2025-07-11 15:37:15 +0200

    Promenjen port na klijentu kako bi mogao komunicirati sa serverom

--- Client/src/client.c
+++ Client/src/client.c
@@ -33,6 +33,11 @@ typedef struct {
     const char* description; //Opis komande
 } Command;
 
+typedef struct {
+    char name[20];
+    char password[20];
+}User;
+
 const Command commands[] = {
     {"Login", "Prijava/Registracija korisnika"},
     {"Logout", "Odjava korisnika"},
@@ -42,6 +47,7 @@ const Command commands[] = {
     {"CheckStatus", "Provera rezervisanih automobila prijavljenog korisnika"},
     {"Reserve[id:ID]", "Rezervacija automobila od strane prijavljenog korisnika po ID-u automobila"}
 };
+
 
 void display_menu(const Command* commands, size_t command_counter)
 {
@@ -55,20 +61,27 @@ void display_menu(const Command* commands, size_t command_counter)
     {
         printf("\t - %s: %s\n", commands[i].name, commands[i].description);
     }
+    puts("\t - Exit: Izlaz iz aplikacije");
     printf("\n\n");
 }
 
-void execute_command(const char* command, const Command* commands, size_t command_count)
+void display_auth_menu()
 {
-    for(size_t i = 0; i < command_count; i++)
-    {
-        if(strcmp(commands[i].name, command) == 0)
-        {
-            printf("Izvrsavanje komande: %s\n", commands[i].name);
-            return;
-        }
-    }
+    puts("\n\t\t\t******************************************");
+    puts("");
+    puts("\t\t\tDobrodosli na stranicu Registar Automobila");
+    puts("");
+    puts("\t\t\t******************************************\n\n\n");
+    puts("Molimo vas da se prijavite ili registrujete:\n");
+    puts("Dostupne komande:\n");
+    puts("\t - Za prijavu korisnika:");
+    puts("\t   # LOGIN [user_name] [password] #");
+    puts("\t - Za registraciju novog korisnika:");
+    puts("\t   # REGISTRATION [name] [surname] [username] [password] #");
+    puts("\t - Exit: Izlaz iz aplikacije");
+    printf("\n\n");
 }
+
 
 int main(int argc , char *argv[])
 {
@@ -76,7 +89,9 @@ int main(int argc , char *argv[])
     int read_size;
     struct sockaddr_in server;
     
+    User user;
     size_t command_count = sizeof(commands) / sizeof(commands[0]);
+    int authenticated = 0; // Flag za proveru da li je korisnik ulogovan
 
     //Create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
@@ -115,7 +130,16 @@ int main(int argc , char *argv[])
 
     while(1)
     {
-        display_menu(commands, command_count);
+        // Prikaz menija na osnovu stanja autentifikacije
+        if(!authenticated)
+        {
+            display_auth_menu();
+        }
+        else
+        {
+            display_menu(commands, command_count);
+        }
+
         printf("Unesite komandu: ");
         if(fgets(message, DEFAULT_BUFLEN, stdin))
             message[strcspn(message, "\n")] = '\0'; // uklanja novi red koji fgets unese  
@@ -125,7 +149,23 @@ int main(int argc , char *argv[])
             continue;
         }
 
-        execute_command(message, commands, command_count);
+        // Provera da li korisnik zeli da izadje
+        if(strcmp(message, "Exit") == 0)
+        {
+            printf("Da li stvarno zelite da izadjete iz aplikacije? (Y/N): ");
+            char exit_choice = getchar();
+            while (getchar() != '\n'); // Čisti ulazni bafer
+            
+            if(exit_choice == 'Y' || exit_choice == 'y')
+            {
+                printf("Zatvaranje veze sa serverom...\n");
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
 
         //Slanje komande serveru
         if( send(sock , message , strlen(message), 0) < 0)
@@ -138,26 +178,62 @@ int main(int argc , char *argv[])
         //Prijem poruke od servera
         if(read_size = recv(sock, message_from_server, DEFAULT_BUFLEN, 0) > 0)
         {
-            //Slanje potvrde serveru da je porukla stigla do klijenta
-            if( send(sock , "PRIMLJENO" , strlen("PRIMLJENO"), 0) < 0)
-            {
-                puts("Send failed");
-                return 1;
-            }
+            // //Slanje potvrde serveru da je porukla stigla do klijenta
+            // if( send(sock , "PRIMLJENO" , strlen("PRIMLJENO"), 0) < 0)
+            // {
+            //     puts("Send failed");
+            //     return 1;
+            // }
 
             printf("Primljena poruka od strane servera:\n");
             printf("%s\n\n", message_from_server);
-            printf("Da li zelite da unesete drugu komandu? (Y/N)");
+
+            // Handle specific LOGIN ERROR responses
+            if(strcmp(message_from_server, "# LOGIN ERROR 101 #") == 0)
+            {
+                printf("GRESKA: Uneli ste pogresnu lozinku!\n\n");
+            }
+            else if(strcmp(message_from_server, "# LOGIN ERROR 102 #") == 0)
+            {
+                printf("GRESKA: Uneli ste pogresno korisnicko ime!\n\n");
+            }
+            else if(strncmp(message_from_server, "# LOGIN SUCCESS", 15) == 0)
+            {
+                printf("Uspesno ste se prijavili!\n\n");
+                authenticated = 1;
+            }
+            // Handle REGISTRATION ERROR responses
+            else if(strcmp(message_from_server, "# REGISTRATION 201 #") == 0)
+            {
+                printf("GRESKA: Korisnik vec postoji!\n\n");
+            }
+            else if(strcmp(message_from_server, "# REGISTRATION 202 #") == 0)
+            {
+                printf("GRESKA: Korisnicko ime je zauzeto!\n\n");
+            }
+            else if(strcmp(message_from_server, "# REGISTRATION SUCCESS #") == 0)
+            {
+                printf("Uspesno ste se registrovali!\n\n");
+                authenticated = 1;
+            }
+
+            if(authenticated)
+            {
+                printf("Da li zelite da unesete drugu komandu? (Y/N): ");
+            }
+            else
+            {
+                printf("Da li zelite da se prijavite/registrujete ponovo? (Y/N): ");
+            }
+            
             char ch = getchar();
             while (getchar() != '\n'); // Čisti ulazni bafer
             if(ch == 'Y' || ch == 'y')
             {
-                free(message_from_server);
                 continue;
             }
             else
             {
-                free(message_from_server);
                 break;
             }
         }
